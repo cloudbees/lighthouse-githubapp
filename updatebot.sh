de2a96d06eeb576ee952d983e83ca497609ccdc3
James Rawlings <rawlingsj80@gmail.com>
2019-10-21 15:44:43 +0100

    switch updatebot to PR dev not prod

--- updatebot.sh
+++ updatebot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jx step create pr chart --name=lighthouse-githubapp --version="${VERSION}" --repo=https://github.com/jenkins-x-bot-test/environment-lordlemon-dev.git
+jx step create pr chart --name=lighthouse-githubapp --version="${VERSION}" --repo=https://github.com/arcalos-environments/environment-raccoonshimmer-dev.git
