223ba60fece3519220bcdff4d27e7dabdf807b78
James Strachan <james.strachan@gmail.com>
2019-10-21 17:50:16 +0100

    fix: use local binary in dockerfile

--- updatebot.sh
+++ updatebot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jx promote -b --all-auto --timeout 1h --version ${VERSION}
+jx promote -b --all-auto --timeout 1h --version ${VERSION} --helm-repo-url=https://storage.googleapis.com/chartmuseum.jenkins-x.io
