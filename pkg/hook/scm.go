ec9ca481cb5fae73b84c74cab896ab926001ba15
James Strachan <james.strachan@gmail.com>
2019-11-29 05:20:20 +0000

    fix: lets cache tokens for a shorter amount
    especially if they are going to expire soon

--- pkg/hook/scm.go
+++ pkg/hook/scm.go
@@ -56,9 +56,9 @@ func (o *HookOptions) getInstallScmClient(log *logrus.Entry, ctx context.Context
 		duration := tokenCacheExpiration
 		if tokenResource.ExpiresAt != nil {
 			// lets use the lowest duration
-			expireDuration := tokenResource.ExpiresAt.Sub(time.Now())
+			expireDuration := tokenResource.ExpiresAt.Sub(time.Now()) - tokenCacheExpireDelta
 			if expireDuration < duration {
-				expireDuration = duration
+				duration = expireDuration
 			}
 		}
 		o.tokenCache.Set(key, tokenResource, duration)
